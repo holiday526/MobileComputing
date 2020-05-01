7ee1a07ac95148cddbc7b989628d38b9f2fdbcc0
holiday526 <taylorchan526@gmail.com>
2020-05-01 16:28:39 +0800

    hot fixed datetime

--- app/Http/Controllers/API/OrdersController.php
+++ app/Http/Controllers/API/OrdersController.php
@@ -47,7 +47,7 @@ class OrdersController extends Controller
         $rules = [
             'items' => 'array|required',
             'address' => 'string|required|min:5',
-            'delivery_time' => 'date_format:Y-m-d\TH:i:sP|required'
+            'delivery_time' => 'date_format:Y-m-d H:i:sP|required'
         ];
         $validator = Validator::make($request->all(), $rules);
         if (count($request->items) <= 0 ) {

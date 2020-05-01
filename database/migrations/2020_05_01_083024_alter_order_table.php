969f45bb23422bf07b9eec96055fa59252a2a719
holiday526 <taylorchan526@gmail.com>
2020-05-01 16:32:40 +0800

    hot fixed

--- database/migrations/2020_05_01_083024_alter_order_table.php
+++ database/migrations/2020_05_01_083024_alter_order_table.php
@@ -15,7 +15,7 @@ class AlterOrderTable extends Migration
     {
         //
         Schema::table('orders', function (Blueprint $table){
-            $table->timestamp('delivery_time')->change();
+            $table->timestamps('delivery_time')->change();
         });
     }
 

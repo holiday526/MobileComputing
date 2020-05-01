b363f60702cbb5bbdb1067de79dff19c40e27f78
holiday526 <taylorchan526@gmail.com>
2020-05-01 16:31:37 +0800

    alter migration

---
+++ database/migrations/2020_05_01_083024_alter_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('orders', function (Blueprint $table){
+            $table->timestamp('delivery_time')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('orders', function (Blueprint $table){
+            $table->timestampTz('delivery_time')->change();
+        });
+    }
+}
